7af45f3e998bc9f40a2c33cb449bf65ea02bc7c1
KR12-raks <koketsorakomane12@gmail.com>
2024-07-03 11:35:31 -0700

    check_9.2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,7 +21,7 @@ int main(void)
 				{
 					putchar(num_1);
 					putchar(num_2);
-					if (num_1 != '8' \\ (num_1 == '8' && num_2 != '9'))
+					if (num_1 != '8' || (num_1 == '8' && num_2 != '9'))
 					{
 						putchar(',');
 						putchar(' ');
