1154ddb0bfe34d1660d9068a449363de3285f3f7
KR12-raks <koketsorakomane12@gmail.com>
2024-07-03 09:28:02 -0700

    check_8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
+/**
+ * main - prints hexadecimal numbers
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char numbers = '0';
+	char letters = 'a';
 
+	while (numbers <= '9')
+	{
+		putchar(numbers);
+		numbers++;
+	}
+	while (letters <= 'f')
+	{
+		putchar(letters);
+		numbers++;
+	}
+	putchar('\n');
+	return (0);
+}
