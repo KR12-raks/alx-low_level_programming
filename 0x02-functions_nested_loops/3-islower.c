cd4c7aa9b603d8edbede4eaf6543a8cafa04d4d0
KR12-raks <koketsorakomane12@gmail.com>
2024-08-01 13:54:43 -0700

    CHeck_3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,13 +9,13 @@ int _islower(int c)
 {
 	char i;
 	int lower = 0;
-	for (i = 'a',i <= 'z',i++)
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		if(i == c)
 		{
-			lower = 1;
+		lower = 1;
 		}
 	}
 	return (lower);
 }
-
