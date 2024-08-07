40e90ff89387d9caee1ffbb478b70962e8f9d92f
KR12-raks <koketsorakomane12@gmail.com>
2024-08-07 07:10:09 -0700

    Check_10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - sum up two integers
+ *
+ * Description: ta add two ints
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: (0)
+ *
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
