736bebe3fa6b0e06e7db58b88f689acd6ef20ec7
KR12-raks <koketsorakomane12@gmail.com>
2024-09-03 14:20:08 -0700

    Check_0

---
+++ 0x03-debbuging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function thatprints if integer is positive or negativie
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i; 
+
+	i = 0;
+	positve_or_negative(i);
+
+	return (0);
+}
